af1e55c21932928bd2a544056d3cdffd03af34e7
Dmitrii Zverev <dima.newmail@gmail.com>
2022-08-30 13:38:53 +1000

    Initial Commit

---
+++ AthleticSwiftUI/AthleticSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthleticSwiftUIApp.swift
+//  AthleticSwiftUI
+//
+//  Created by Dmitrii Zverev on 30/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct AthleticSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
